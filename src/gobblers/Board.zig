ee815d8ee0213873913bc0b5ef552d569084efb2
seppelin <seppelin@gmx.de>
2023-12-16 22:29:14 +0100

    init Selection

--- src/gobblers/Board.zig
+++ src/gobblers/Board.zig
@@ -5,25 +5,25 @@ pub const Move = packed struct {
     to_pos: u4,
 };
 
-const Pos = @This();
+const Board = @This();
 
 layers: [2][3]u9,
 pieces: [2][3]u2,
 next_sign: u1,
 moves: i8,
 
-pub fn init() Pos {
-    return Pos{ .layers = .{
+pub fn init() Board {
+    return Board{ .layers = .{
         .{ 0, 0, 0 },
         .{ 0, 0, 0 },
     }, .pieces = .{ .{ 2, 2, 2 }, .{ 2, 2, 2 } }, .next_sign = 0, .moves = 0 };
 }
 
-pub fn signTopView(self: Pos, sign: u1) u9 {
+pub fn signTopView(self: Board, sign: u1) u9 {
     return self.layers[sign][0] & ~self.layers[~sign][1] | self.layers[sign][1] & ~self.layers[~sign][2] | self.layers[sign][2];
 }
 
-pub fn biggerView(self: Pos, size: u2) u9 {
+pub fn biggerView(self: Board, size: u2) u9 {
     return switch (size) {
         0 => self.layers[0][1] | self.layers[1][1] |
             self.layers[0][2] | self.layers[1][2],
@@ -33,7 +33,7 @@ pub fn biggerView(self: Pos, size: u2) u9 {
     };
 }
 
-pub fn getKey(self: *Pos) u48 {
+pub fn getKey(self: *Board) u48 {
     var key: u48 = 0;
     var sign: u6 = 0;
     while (sign < 2) : (sign += 1) {
@@ -54,7 +54,7 @@ pub fn getKey(self: *Pos) u48 {
     return key;
 }
 
-pub fn doMove(self: *Pos, move: Move) void {
+pub fn doMove(self: *Board, move: Move) void {
     // From
     switch (move.new) {
         true => self.pieces[self.next_sign][move.size] -= 1,
@@ -67,7 +67,7 @@ pub fn doMove(self: *Pos, move: Move) void {
     self.moves += 1;
 }
 
-pub fn undoMove(self: *Pos, move: Move) void {
+pub fn undoMove(self: *Board, move: Move) void {
     self.moves -= 1;
     self.next_sign = ~self.next_sign;
 
@@ -80,48 +80,48 @@ pub fn undoMove(self: *Pos, move: Move) void {
     self.layers[self.next_sign][move.size] ^= (1 << move.to_pos);
 }
 
-pub fn doNewMove(self: *Pos, size: u2, to_pos: u4) void {
+pub fn doNewMove(self: *Board, size: u2, to_pos: u4) void {
     self.pieces[self.next_sign][size] -= 1;
     self.layers[self.next_sign][size] |= (1 << to_pos);
     self.next_sign = ~self.next_sign;
     self.moves += 1;
 }
 
-pub fn doBoardMove(self: *Pos, size: u2, from_pos: u4, to_pos: u4) void {
+pub fn doBoardMove(self: *Board, size: u2, from_pos: u4, to_pos: u4) void {
     self.pieces[self.next_sign][size] ^= (1 << from_pos);
     self.layers[self.next_sign][size] |= (1 << to_pos);
     self.next_sign = ~self.next_sign;
     self.moves += 1;
 }
 
-pub fn undoNewMove(self: *Pos, size: u2, to_pos: u4) void {
+pub fn undoNewMove(self: *Board, size: u2, to_pos: u4) void {
     self.moves -= 1;
     self.next_sign = ~self.next_sign;
     self.layers[self.next_sign][size] ^= (1 << to_pos);
     self.pieces[self.next_sign][size] += 1;
 }
 
-pub fn undoBoardMove(self: *Pos, size: u2, from_pos: u4, to_pos: u4) void {
+pub fn undoBoardMove(self: *Board, size: u2, from_pos: u4, to_pos: u4) void {
     self.moves -= 1;
     self.next_sign = ~self.next_sign;
     self.layers[self.next_sign][size] ^= (1 << to_pos);
     self.layers[self.next_sign][size] |= (1 << from_pos);
 }
 
-pub fn isNewLeft(self: Pos, size: u2) bool {
+pub fn isNewLeft(self: Board, size: u2) bool {
     return self.pieces[self.next_sign][size] != 0;
 }
 
-pub fn isMoveable(self: Pos, size: u2, pos: u4) bool {
+pub fn isMoveable(self: Board, size: u2, pos: u4) bool {
     var move = self.layers[self.next_sign][size] & (1 << pos);
     return (move & self.biggerView(size)) == 0;
 }
 
-pub fn isFree(self: Pos, size: u2, pos: u4) bool {
+pub fn isFree(self: Board, size: u2, pos: u4) bool {
     return ((1 << pos) & self.biggerView(size)) == 0;
 }
 
-pub fn getMoves(self: *const Pos, buf: *[42]Move) u8 {
+pub fn getMoves(self: *const Board, buf: *[42]Move) u8 {
     var len: u8 = 0;
     var size: u2 = 0;
     var size_view: u9 = 0;
@@ -145,7 +145,7 @@ pub fn getMoves(self: *const Pos, buf: *[42]Move) u8 {
     return len;
 }
 
-fn addMoves(self: *const Pos, new: bool, size: u2, from_pos: u4, buf: *[42]Move, len: *u8) void {
+fn addMoves(self: *const Board, new: bool, size: u2, from_pos: u4, buf: *[42]Move, len: *u8) void {
     var to_pos: u4 = 0;
     var bigger_view: u24 = undefined;
     while (to_pos < 9) : (to_pos += 1) {
@@ -174,7 +174,7 @@ fn checkView(view: u9) u2 {
     return @intFromBool(check != 0);
 }
 
-pub fn getScore(self: *const Pos) i8 {
+pub fn getScore(self: *const Board) i8 {
     var edited = checkView(self.signTopView(self.next_sign));
     edited |= checkView(self.signTopView(~self.next_sign)) << 1;
 
@@ -186,8 +186,50 @@ pub fn getScore(self: *const Pos) i8 {
     };
 }
 
-pub const Selection = union(enum) {
-    none: .{},
-    from: .{},
-    both: .{},
+pub const Selection = struct {
+    pub const State = enum {
+        none,
+        from,
+        both,
+    };
+
+    state: State,
+    move: Move,
+
+    pub fn init() Selection {
+        return Selection{
+            .state = .null,
+            .move = undefined,
+        };
+    }
+
+    pub fn selectNew(self: *Selection, board: Board, size: u2) void {
+        if (board.isNewLeft(size)) {
+            self.state = .from;
+            self.move.new = true;
+            self.move.size = 2;
+        }
+    }
+
+    pub fn selectBoard(self: *Selection, board: Board, size: u2, pos: u4) void {
+        switch (self.state) {
+            .none => {
+                if (board.isMoveable(size, pos)) {
+                    self.state = .from;
+                    self.move.new = false;
+                    self.move.size = size;
+                    self.move.from_pos = pos;
+                }
+            },
+            .from => {
+                if (board.isFree(self.move.size, pos)) {
+                    self.state = .both;
+                    self.move.to_pos = pos;
+                } else {
+                    self.state = .none;
+                }
+            },
+            .both => {},
+        }
+    }
 };
